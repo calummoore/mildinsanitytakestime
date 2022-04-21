c01448d2427060e3567b21c2ecfacadf385f7a13
Calum Moore <calum.moore@gmail.com>
2022-04-21 02:13:43 +0100

    Add twitter logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import { ReactChild } from 'react'
-import { Box, HStack, Stack, Spacer, Flex, IconButton, Button, useBreakpointValue } from '@chakra-ui/react'
+import { Box, HStack, Stack, Spacer, Flex, Circle, IconButton, Button, useBreakpointValue, Avatar } from '@chakra-ui/react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -7,6 +7,7 @@ import { Footer } from './Footer'
 import { Sidebar } from './Sidebar'
 import { Logo } from './Logo'
 import lines from '../img/lines.svg'
+import avatar from '../img/bear.png'
 import { useRouter } from 'next/router'
 
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL
@@ -23,17 +24,35 @@ export function Layout ({ children, name, desc }: LayoutProps) {
   const isMobile = useBreakpointValue([true, true, false])
   const router = useRouter()
 
-  const navEl = NAV.map(({ title, link }) => {
-    return (
-      <Box  key={title} color={router.route.startsWith(link) ? 'brand.600' : undefined} _hover={{
-        color: 'brand',
-      }}>
-        <Link href={link}>
-          {title}
-        </Link>
-      </Box>
-    )
-  })
+  // const navEl = NAV.map(({ title, link }) => {
+  //   return (
+  //     <Box  key={title} color={router.route.startsWith(link) ? 'brand.600' : undefined} _hover={{
+  //       color: 'brand',
+  //     }}>
+  //       <Link href={link}>
+  //         {title}
+  //       </Link>
+  //     </Box>
+  //   )
+  // })
+
+  const navEl = (
+    <Link href='https://twitter.com/calummoore'>
+      <a target='_blank'>
+        <Button
+          variant='ghost'
+          color='gray.700'
+          leftIcon={(
+            <Circle size='30px' overflow='hidden'>
+              <Image width={30} height={30}  src={avatar} alt='Calum' />
+            </Circle>
+          )}
+        >
+      Follow on Twitter
+        </Button>
+      </a>
+    </Link>
+  )
 
   const canonicalTitle = `${name} | Magical request queues for serverless`
   const canonicalDescription = `${desc}. Schedule, queue, delay and/or asynchronous serverless functions or API endpoints. Get setup in under 5 minutes.`
